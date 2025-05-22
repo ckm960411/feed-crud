d4fd6607250585f7da86fa050c52efec5ff0a8b9
ckm960411 <a01098956117@gmail.com>
2025-05-22 15:35:42 +0900

    로그인한 유저정보 조회

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/entities/user.entity';
+import { User as UserDecorator } from 'src/auth/decorators/user.decorator';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  async getMe(@UserDecorator() user: User) {
+    return user;
+  }
+}

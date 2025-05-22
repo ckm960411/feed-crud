31073c58ab4fe93ca9cd268f787763b8b608fe6d
ckm960411 <a01098956117@gmail.com>
2025-05-22 14:53:27 +0900

    CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true, // class-validator 데코레이터가 없는 속성은 무시
+      errorHttpStatusCode: 400,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => ({
+          field: error.property,
+          value: error.value,
+          message: Object.values(error.constraints)[0],
+        }));
+
+        throw new BadRequestException({
+          statusCode: 400,
+          message: '잘못된 요청입니다',
+          errors: messages,
+        });
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
